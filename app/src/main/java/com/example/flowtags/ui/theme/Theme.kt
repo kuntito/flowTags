b769b70bc1836f391df8f9500c7f813c1c211ad2
kuntito <owolabitito@gmail.com>
2026-05-20 15:25:21 +0100

    initial commit

---
+++ app/src/main/java/com/example/flowtags/ui/theme/Theme.kt
@@ -0,0 +1,44 @@
+package com.example.flowtags.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+private val DarkColorScheme = darkColorScheme(
+    background = colorDeep,
+    onBackground = colorFluster,
+)
+
+@Composable
+fun FlowTagsTheme(
+    content: @Composable () -> Unit
+) {
+    val colorScheme = DarkColorScheme
+
+    // sets up status bar color
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorPraise.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
